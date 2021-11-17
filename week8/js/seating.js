291d83ebedcc4108b9e3f149c745bc85a237a146
John Rowley <outsidethebox@eircom.net>
2021-11-17 20:57:04 +0000

    final code with seating selection

--- week8/js/seating.js
+++ week8/js/seating.js
@@ -5,14 +5,47 @@ $(function () {
 
 function initJquerySeating() {
 
+    const maxSeatsChosen = 3;
 
-    $('.seat').on('click', function() {
+    $('.seat').on('click', function () {
 
-const row = $(this).data("row");
-const col = $(this).data("col");
+        if ($(this).hasClass('reserved')) return;
 
-$('#seatsChosen').html(`selected seat: ${row}-${col}`);
 
+        if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+            $('#seatsChosen').html(``);
+            ShowSeatCount();
+            return;
+        }
+
+        if ($('.selected').length >= maxSeatsChosen) { 
+            return; 
+        } 
+
+        $(this).addClass('selected');
+    
+        ShowSeatCount();
+    
     })
- 
+
+}
+
+function ShowSeatCount() {
+
+    let seatsChosen = [];
+
+    $('.selected').each( function (index) {
+
+        const rowSeat = $(this).data("row");
+        const colSeat = $(this).data("col");
+
+        seatsChosen.push( {row: rowSeat, col: colSeat} );
+
+    });
+
+    let chosenSeats = seatsChosen.map(x => { return `${x.row}:${x.col}` } );
+
+    $('#seatsChosen').html(`selected seats: ${chosenSeats.join(', ')}`);
+
 }
